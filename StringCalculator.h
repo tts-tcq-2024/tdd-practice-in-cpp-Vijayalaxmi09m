52aafc531ebc7dbeb781bad0e159f8c99d8ff65d
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-09-18 19:34:06 +0530

    first test case

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,13 @@
+// StringCalculator.h
+#ifndef STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
+
+#include <string>
+
+class StringCalculator {
+public:
+    int add(const std::string& input);
+};
+
+#endif // STRING_CALCULATOR_H
 
