688fc18f9142bfff47f86a64f4c94e9ca8c42a56
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-09-18 19:34:46 +0530

    first test case

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,10 @@
+#include "StringCalculator.h"
 
+int StringCalculator::add(const std::string& input) {
+    if (input.empty()) {
+        return 0; // Return 0 for empty input
+    }
+
+    // Future cases will handle more logic here
+    return 0;
+}
